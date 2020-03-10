42d672089d003b6939cc1f417f059a6b9a4526bc
André Caiado <andrecaiado@gmail.com>
2020-03-10 18:44:32 +0000

    End of section 3

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -20,8 +20,9 @@ namespace DatingApp.API.Controllers
             _context = context;
 
         }
+
         // GET api/values
-        
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
